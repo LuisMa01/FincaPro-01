c99f35075ed133c1af4ed27e3f946fa537293875
LuisMa <lmanudr01@gmail.com>
2022-09-03 08:47:41 -0400

    Backend

--- server.js
+++ server.js
@@ -1,102 +1,234 @@
-const express = require('express')
-const methodOverride = require('method-override')
+const express = require("express");
+//const methodOverride = require("method-override");
+const cors = require("cors");
+const { sign } = require("jsonwebtoken");
+const { serialize } = require("cookie");
 
-const { pool } = require('./models/db-conect')
-const bcrypt = require("bcrypt")
-const session = require("express-session")
-const passport = require("passport")
-const flash = require('express-flash')
-const expressLayouts = require('express-ejs-layouts')
+const { pool } = require("./models/db-conect");
+const bcrypt = require("bcrypt");
+//const session = require("express-session");
+//const passport = require("passport");
 
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const app = express();
 
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const app = express()
+app.use(express.json());
 
-//
-
-//setting
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-
-
-//Middlewares
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({ extended: true}))
-app.use(cookieParser(process.env.SECRETO))
-app.use(flash())
-
-
-app.use(session({
+//app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+/*
+app.use(
+  session({
     secret: process.env.SECRETO,
     resave: true,
-    saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-
-const initializePassport = require('./config/passport-config')
-
-
-initializePassport(passport)
-
-
-
-//metodo y ruta visitada
-app.use((req,res,next) => {
-    console.log(`${req.method} ${req.path}`)
-    next()
-})
-/* nota de como hacer query
-const ti = 'user'
-const te = '1234'
-const consulta = pool.query('select * from "userSchema"."User" WHERE user_name = $1', [ti], (err, results) => {
-    if(err){
-        throw err
-    }
-    console.log(results.rows[0].user_name)
-    console.log(results.rows[0].password)
-    const hash = bcrypt.hashSync(te, 10);
-    
-    let values = [hash, results.rows[0].user_name]
-    pool.query('UPDATE "userSchema"."User" SET password= $1 WHERE user_name= $2;', values, (err, res) =>{
-      if(err){
-        throw err
-      }
-      console.log(results.rows[0].user_name)
-      console.log(res.rows)
-      
-    })
-    
-    
-})
+    saveUninitialized: true,
+  })
+);
 */
-/*
-const teta = pool.query('SELECT * FROM "userSchema".user_rol ORDER BY rol_id ASC ',
-(err, results) => {
-    if (err) {
-      throw err;
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+//const initializePassport = require("./config/passport-config");
+
+//initializePassport(passport);
+
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5000","*"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
+  },
+  method: ['GET', 'PUT', 'POST'],
+  Credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-  console.log(results.rows.length)
-  console.log('hasta aqui')
-})
+app.use(cors(corsOptions));
 
-*/
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
 
-
-
-
+const secreto = process.env.SECRETO;
 
 //Proceso de login
 
+app.post("/login/user", (req, res) => {
+  // check if user that match req.email and req.password return user user_id, user_name, user_rol
+
+  const { user, password, isEmail } = req.body;
+
+  console.log(user + password + isEmail);
+  console.log(req.body.user);
+  if (isEmail) {
+    pool.query(
+      'SELECT * FROM "userSchema"."User" WHERE email = $1',
+      [user],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        if (results.rows.length > 0) {
+          const userDB = results.rows[0];
+          if (bcrypt.compareSync(password, userDB.password)) {
+            //req.session.user = user;
+            // res user
+            //COMIENZA EL TOKEN
+            const token = sign(
+              {
+                exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 1, // 30 days
+                username: userDB.user_name,
+              },
+              secreto
+            );
+
+            const serialised = serialize("OursiteJWT", token, {
+              httpOnly: true,
+              secure: process.env.NODE_ENV !== "development",
+              sameSite: "strict",
+              maxAge: 60 * 60 * 24 * 1,
+              path: "/",
+            });
+            
+          
+
+            res.setHeader("Set-Cookie", serialised);
+
+            res.status(200).json({ message: "Success!" });
+
+            //TERMINA EL TOKEN
+
+            //res.send({ data: userDB });
+          } else {
+            res.status(401).send({ message: "Unauthorized" });
+          }
+        } else {
+          res.status(404).send({ message: "No found" });
+        }
+      }
+    );
+  } else {
+    pool.query(
+      'SELECT * FROM "userSchema"."User" WHERE user_name = $1',
+      [user],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        if (results.rows.length > 0) {
+          const userDB = results.rows[0];
+          if (bcrypt.compareSync(password, userDB.password)) {
+            //req.session.user = user;
+            // res user
+            //COMIENZA EL TOKEN
+            const token = sign(
+              {
+                exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 1, // 1 days
+                username: userDB.user_name,
+              },
+              secreto
+            );
+            
+
+            const serialised = serialize("OursiteJWT", token, {
+              httpOnly: true,
+              secure: process.env.NODE_ENV !== "development",
+              sameSite: "Lax",
+              maxAge: 60 * 60 * 24 * 1,
+              path: "/",
+            });
+            
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.setHeader('Access-Control-Allow-Credentials', true)
+            res.setHeader("Set-Cookie", serialised);
+            
+            
+
+            res.status(200).json({ message: "Success!"});
+
+            //TERMINA EL TOKEN
+            //res.send({ data: userDB });
+          } else {
+            res.status(401).send({ message: "Unauthorized" });
+          }
+        } else {
+          res.status(404).send({ message: "No found" });
+        }
+      }
+    );
+  }
+});
+
+app.get("/logout", (req, res) => {
+  const { cookies } = req;
+
+  const jwt = cookies.OursiteJWT;
+
+  if (!jwt) {
+    res.json({ message: "Ya no estás logeado." });
+  } else {
+    const serialised = serialize("OursiteJWT", null, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: -1,
+      path: "/",
+    });
+
+    res.setHeader("Set-Cookie", serialised);
+
+    res.status(200).json({ message: "Successfuly logged out!" });
+  }
+});
+
+
+
+
+app.get("/user", (req, res) => {
+ 
+  pool.query(
+    'SELECT * FROM "userSchema"."User" ORDER BY user_id ASC',
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      const token = sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 1, // 1 days
+          username: 'loco',
+        },
+        secreto
+      );
+      
+
+      const serialised = serialize("OursiteJWT", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "Lax",
+        maxAge: 60 * 60 * 24 * 1,
+        path: "/",
+      });
+      
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
+      res.setHeader("Set-Cookie", serialised);
+      
+
+      res.send(results.rows);
+    }
+  );
+});
+
+/*
 app.get('/auth/login', checkNotAuthenticated, (req, res) => {
 
-    res.render('auth/login',{ layout: './auth/login' })
+    res.render()
     
 })
 
@@ -105,13 +237,13 @@ app.post('/auth/login', checkNotAuthenticated, passport.authenticate("local", {
     failureRedirect: '/auth/login',
     failureFlash: true
 }))
+app.post('/auth/login', checkNotAuthenticated, passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
+    res.redirect('/');
+  });
 
-app.get("/auth/logout", (req, res) => {
-  
-  //res.clearCookie();
-  res.clearCookie('connect.sid')
 
-    
+app.get("/auth/logout", (req, res) => {  
+  res.clearCookie('connect.sid') 
   
   req.logout
   res.render("./auth/login", { message: "Saliste de sesión exitosamente!", layout: './auth/login'});
@@ -119,12 +251,16 @@ app.get("/auth/logout", (req, res) => {
 
 
 //Acceso a la app con autenticacion
+
 const indexRouter = require('./routes/index')
 app.use('/', checkAuthenticated, indexRouter)
 
+
 //Acceso update password
+
 const userRouter = require('./routes/user')
 app.use('/', checkAuthenticated, userRouter)
+
 
 
 
@@ -143,5 +279,6 @@ function checkNotAuthenticated(req, res, next) {
     }
     next()
 }
+*/
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
