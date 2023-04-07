04f958f5101a4157863bc94e0ec7d38fde301f51
LuisMa <lmanudr01@gmail.com>
2023-04-07 13:56:35 -0400

    viernes santo

--- controllers/appController.js
+++ controllers/appController.js
@@ -9,7 +9,7 @@ const asyncHandler = require("express-async-handler");
 const getAllApps = asyncHandler(async (req, res) => {
   pool
     .query(
-      "SELECT date_init, date_end, date_act_key, date_crop_key, date_id, date_user_key, crop_camp_key, crop_plant_key, crop_name, user_name, user_nombre, act_name, camp_name	FROM public.table_app_date INNER JOIN public.table_crop ON crop_id = date_crop_key INNER JOIN public.table_activity ON act_id = date_act_key INNER JOIN public.table_camp ON camp_id = crop_camp_key INNER JOIN public.table_user ON user_id = date_user_key ORDER BY date_id ASC;"
+      "SELECT date_init, date_end, date_act_key, date_crop_key, date_id, date_user_key, crop_camp_key, crop_plant_key, crop_name, crop_harvest, crop_status, user_name, user_nombre, act_name, camp_name	FROM public.table_app_date INNER JOIN public.table_crop ON crop_id = date_crop_key INNER JOIN public.table_activity ON act_id = date_act_key INNER JOIN public.table_camp ON camp_id = crop_camp_key INNER JOIN public.table_user ON user_id = date_user_key ORDER BY date_id ASC;"
     )
     .then((results) => {
       //res.send(results.rows)
