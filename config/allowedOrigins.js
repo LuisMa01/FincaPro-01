4ea5103d39778eb07648e7b6dd011c7c130512fc
LuisMa <lmanudr01@gmail.com>
2023-05-02 15:41:04 -0400

    martes de amanecidav2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://127.0.0.1:5173", "http://localhost:5173", "http://localhost:5000"] 
+const allowedOrigins = ["https://andre-vloebergh.onrender.com", "https://andre-vloebergh-api.onrender.com"] 
 //["https://andre-vloebergh.onrender.com", "https://andre-vloebergh-api.onrender.com", "http://localhost:3000", "http://localhost:5000", "http://127.0.0.1:5173", "http://localhost:5173"];
 
 module.exports = allowedOrigins;
