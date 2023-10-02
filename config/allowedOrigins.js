a41b216a61977de4d62fd9ff943420c60af39a75
LuisMa <lmanudr01@gmail.com>
2023-10-02 13:34:10 -0400

    completo front 1.5

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 //URL permitidas por Cors
-const allowedOrigins = ["https://andre-vloebergh.onrender.com", "https://andre-vloebergh-api.onrender.com"] 
-//["https://andre-vloebergh.onrender.com", "https://andre-vloebergh-api.onrender.com", "http://localhost:3000", "http://localhost:5000", "http://127.0.0.1:5173", "http://localhost:5173"];
+const allowedOrigins = process.env.ALLOWED_HOST
+//["https://andre-vloebergh.onrender.com", "https://andre-vloebergh-api.onrender.com", "http://localhost:3000", "http://localhost:5000", "http://127.0.0.1:5173", "http://localhost:5173"]; "http://127.0.0.1:5000", "http://localhost:5000", "http://127.0.0.1:8080", "http://localhost:8080"
 
 module.exports = allowedOrigins;
